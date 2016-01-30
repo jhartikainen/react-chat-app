085e8aa53b26dd065e75d9f2ead1b55222525adb
Jani Hartikainen <jani@jjh.fi>
2016-01-30 22:24:25 +0200

    Add MessageForm component

--- src/Chat.js
+++ src/Chat.js
@@ -1,37 +1,25 @@
 var React = require('react');
 
 var MessageList = require('./MessageList');
+var MessageForm = require('./MessageForm');
 
 module.exports = React.createClass({
 	getInitialState: function() {
 		return {
-			input: '',
 			messages: []
 		};
 	},
 
-	submit: function(ev) {
-		ev.preventDefault();
-
-		var newMessage = this.state.input;
-
+	onSend: function(newMessage) {
 		this.setState({
 			messages: this.state.messages.concat([newMessage]),
-			input: ''
 		});
-	},
-
-	updateInput: function(ev) {
-		this.setState({ input: ev.target.value });
 	},
 
 	render: function() {
 		return <div>
 			<MessageList messages={this.state.messages} />
-			<form onSubmit={this.submit}>
-				<input value={this.state.input} onChange={this.updateInput} type="text" />
-				<input type="submit" value="Send" />
-			</form>
+			<MessageForm onSend={this.onSend} />
 		</div>;
 	}
 });
---
+++ src/MessageForm.js
@@ -0,0 +1,30 @@
+var React = require('react');
+
+module.exports = React.createClass({
+	getInitialState: function() {
+		return {
+			input: ''
+		};
+	},
+
+	submit: function(ev) {
+		ev.preventDefault();
+
+		this.props.onSend(this.state.input);
+
+		this.setState({
+			input: ''
+		});
+	},
+
+	updateInput: function(ev) {
+		this.setState({ input: ev.target.value });
+	},
+
+	render: function() {
+		return <form onSubmit={this.submit}>
+			<input value={this.state.input} onChange={this.updateInput} type="text" />
+			<input type="submit" value="Send" />
+		</form>;
+	}
+});
